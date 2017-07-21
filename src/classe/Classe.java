986fe599b6664b89c77d3032ab66344acd7dba1c
andrechateau <dendemw@gmail.com>
2017-07-21 19:52:05 -0300

    adi¸c˜ao do atributo a3

--- src/classe/Classe.java
+++ src/classe/Classe.java
@@ -1,29 +1,33 @@
 package classe;
 
-
 public class Classe {
 
     private int A1;
     private float A2;
-
+    private String A3;
 
     public int getA1() {
         return A1;
     }
 
-
     public void setA1(int A1) {
         this.A1 = A1;
     }
 
- 
     public float getA2() {
         return A2;
     }
 
-
     public void setA2(float A2) {
         this.A2 = A2;
+    }
+
+    public String getA3() {
+        return A3;
+    }
+
+    public void setA3(String A3) {
+        this.A3 = A3;
     }
 
 }
