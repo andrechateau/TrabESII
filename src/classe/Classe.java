c17a5a652f0696c936c8aef8483fb1f20fb81c38
andrechateau <dendemw@gmail.com>
2017-07-21 19:49:02 -0300

    Versao 1.0

--- src/classe/Classe.java
+++ src/classe/Classe.java
@@ -1,38 +1,27 @@
 package classe;
 
-/**
- *
- * @author Andre Chateaubriand
- */
+
 public class Classe {
 
     private int A1;
     private float A2;
 
-    /**
-     * @return the A1
-     */
+
     public int getA1() {
         return A1;
     }
 
-    /**
-     * @param A1 the A1 to set
-     */
+
     public void setA1(int A1) {
         this.A1 = A1;
     }
 
-    /**
-     * @return the A2
-     */
+ 
     public float getA2() {
         return A2;
     }
 
-    /**
-     * @param A2 the A2 to set
-     */
+
     public void setA2(float A2) {
         this.A2 = A2;
     }
