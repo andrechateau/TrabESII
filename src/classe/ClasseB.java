51c3b38c1e043b0c22c521d9d160760d72022f65
andrechateau <dendemw@gmail.com>
2017-07-21 20:25:26 -0300

    Classe B com atributo b4

---
+++ src/classe/ClasseB.java
@@ -0,0 +1,16 @@
+package classe;
+
+public class ClasseB {
+
+    private String B4;
+    private float B2;
+
+    public String getB1() {
+        return B4;
+    }
+
+    public void setB1(String B1) {
+        this.B4 = B4;
+    }
+
+}
